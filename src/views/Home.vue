fb81dd77530856340db05fd7d38021d7e351e23c
Yaşar İdikut <yasar8000@gmail.com>
2024-04-18 17:40:25 -0400

    add more videos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,33 @@
       Heuristic 1 + ORCA + Deadlock Resolution Demo!
     </p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/Fwy_5q_lkCc?si=f2S7TXlu_h8KGwp8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <p class="mt-5">
+      MQP trailer!
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qo-FxxT3i9I?si=ZwVZQsm1Or8yNvoo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <p class="mt-5">
+      Heuristic1 + Recalc Full Demo!
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/V9ycTuafOX4?si=MW9DSdsO1TOV5QEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <p class="mt-5">
+      1st working demo (birds eye view)!
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qBHlO6XKXPU?si=Ub6JxwtnXRIW770F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <p class="mt-5">
+      MQP Demo In Progress!
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/WjcR9yUZi2g?si=e-G56j7RHUu-T4yI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <p class="mt-5">
+      Real World + ARGoS Annotated run
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/c43oxJ5FNB8?si=PBWetEK5rXirkags" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
   </div>
 </template>
 
