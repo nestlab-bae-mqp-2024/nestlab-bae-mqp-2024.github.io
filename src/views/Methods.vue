d7c5236e82bf0fa3fe167ad26dd853ff5d5bcac4
Yaşar İdikut <yasar8000@gmail.com>
2024-04-19 08:46:44 -0400

    update methods

--- src/views/Methods.vue
+++ src/views/Methods.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-xl m-10">
     <p class="mb-5">
-      Beep Bop Beep Bop... Under construction!
+      Here, you can find out how we are impl
     </p>
 
     <p class="mb-5">
@@ -9,7 +9,11 @@
     </p>
 
     <p class="mb-5">
-      Explanation
+      We have a Jupyter Notebook for this! Click <a class="underline hover:no-underline" target="_blank" href="https://github.com/wpi-nest-lab-bae-mqp-2023/milp-intro/blob/main/src/mrpcp_2015/attempts-and-improvements/2015_mrpcp_yasars_attempt_w_redundancy_param.ipynb">here</a> to see.
+    </p>
+    <p class="mb-5">
+      This is the optimal solver for our problem, but it takes days or weeks to solve until optimality.
+      In the case we present (8 robots, 64 nodes, ...), it took my computer 2.5 days to get to a solution with an optimality gap of <8%.
     </p>
 
     <p class="mb-5">
@@ -17,7 +21,10 @@
     </p>
 
     <p class="mb-5">
-      Explanation
+      We have a Jupyter Notebook for this! Click <a class="underline hover:no-underline" target="_blank" href="https://github.com/wpi-nest-lab-bae-mqp-2023/milp-intro/blob/main/src/heuristic_attempts/yasars_heuristic_attempts/yasars_heuristic_attempt.ipynb">here</a> to see.
+    </p>
+    <p class="mb-5">
+      This is the drop-in replacement for MILP.
     </p>
 
     <p class="mb-5">
@@ -25,7 +32,7 @@
     </p>
 
     <p class="mb-5">
-      Explanation
+      TODO: Explanation
     </p>
 
   </div>
