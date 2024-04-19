f66322fe72ead84ac62c567c792d2d8d849dccc1
Yaşar İdikut <yasar8000@gmail.com>
2024-04-19 13:29:30 -0400

    update home and results

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,56 @@
       Here, you will find a quick summary of our undergraduate capstone project (MQP).
     </p>
 
+    <p class="text-4xl m5-10">
+      <strong>Multi-Robot Persistent Coverage Under Fuel and Stochastic Failure Constraints</strong>
+    </p>
+
+    <p class="text-xl m5-10 my-10">
+      <strong>Abstract: </strong>
+      The guiding application of this research project is multi-robot systems as they relate to agricultural challenges, especially in the persistent inspection of cropland. In this research project, we present both a baseline (MILP) and novel algorithms to solve a multi-robot coverage problem under battery depletion and stochastic failures. We compare the solutions based on metrics about visitation and redundancy. Finally, we present a visualization, a simulation, and a real-world demonstration (using Khepera IV robots) setup to provide additional data to analyze our solution.
+    </p>
+
+    <p class="text-xl m5-10 5 text-center">
+      <strong>Students: </strong>
+      Yaşar İdikut, Camden Cummings, Samara Holmes
+    </p>
+    <p class="text-xl m5-10 5 text-center">
+      <strong>Advisor: </strong>
+      Carlo Pinciroli
+    </p>
+
     <div>
+      <p class="mt-5 text-center">
+        Summary demo video
+      </p>
       <div style="position:relative;padding-top:56.25%;">
         <iframe src="https://www.youtube.com/embed/gj_nY3BjXEs?si=6ZmWuWsSXYtflNm2?rel=0&autoplay=1" frameborder="0" allowfullscreen
                 style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
       </div>
     </div>
 
+    <p class="text-xl m5-10 my-10 text-center">
+      <strong>Presentation Slides: </strong>
+      <a href="/WPI_MQP_MRPCUFSFC.pptx" class="underline hover:no-underline" target="_blank"><strong>here</strong></a>
+      <strong class="ml-10">Presentation Poster: </strong>
+      <a href="/MQP_Poster.pdf" class="underline hover:no-underline" target="_blank"><strong>here</strong></a>
+    </p>
 
-    <p class="mb-5">
+    <p class="my-5">
+      TODO:
       For a more detailed explanation of the methods used, <router-link class="underline hover:no-underline" to="/methods"><strong>click here</strong></router-link>.
+    </p>
+
+    <p class="my-5">
+      TODO:
       For a more detailed explanation of our results, <router-link class="underline hover:no-underline" to="/results"><strong>click here</strong></router-link>.
+    </p>
+
+    <p class="my-5">
       To contact us, <router-link class="underline hover:no-underline" to="/contact"><strong>click here</strong></router-link>.
     </p>
+
+
 
 
   </div>
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,53 +1,120 @@
 <template>
 
-  <div class="text-xl m-10 content-center">
+  <p class="text-xl">
+    <strong>Graphs</strong>
+  </p>
+
+  <p class="mt-5">
+    Please refer to <a href="/WPI_MQP_MRPCUFSFC.pptx" class="underline hover:no-underline" target="_blank"><strong>slides</strong></a> and <a href="/MQP_Poster.pdf" class="underline hover:no-underline" target="_blank"><strong>poster</strong></a> for now.
+  </p>
+
+  <p class="text-xl mt-5">
+    <strong>Extended versions of the videos in the demo</strong>
+  </p>
+
+  <div>
     <p class="mt-5 text-center">
-      Full Run for Simulation
+      Full Run (3rd) for Simulation
     </p>
-    <iframe class="" width="560" height="315" src="https://www.youtube.com/embed/Ly4jAxPle3E?si=wuIvM1wyaD3GpiXG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/Ly4jAxPle3E?si=wuIvM1wyaD3GpiXG" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
   </div>
-  <div class="text-xl m-10 content-center">
+
+
+  <div>
     <p class="mt-5 text-center">
-      Full Run for Real World (Birds Eye + ARGoS Annotated)
+      Full Run (3rd) for Real World (Birds Eye + ARGoS Annotated)
     </p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/c43oxJ5FNB8?si=PBWetEK5rXirkags" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/c43oxJ5FNB8?si=PBWetEK5rXirkags" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
   </div>
 
-  <div class="text-xl m-10">
-    <p class="mb-5">
-      Below are the rest of the videos showing how our simulation and real world setup looked like.
+  <p class="text-xl mt-10">
+    <strong>More runs of the same algorithm</strong>
+  </p>
+
+  <div>
+    <p class="mt-5 text-center">
+      1st working demo (birds eye view only)!
     </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/qBHlO6XKXPU?si=Ub6JxwtnXRIW770F" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
   </div>
 
-  <p class="mt-96">
-    Heuristic 1 + ORCA + Stochastic Failure Demo!
-  </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/JnMw7biMyI8?si=eDI_XFZrclZnSJch" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <div>
+    <p class="mt-5 text-center">
+      2nd working demo (ARGoS view only)!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/AAcHfXZ8Mdo?si=GjrExbljQ0H_w8Sv" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
 
-  <p class="mt-5">
-    Heuristic 1 + ORCA + Deadlock Resolution Demo!
+  <p class="text-xl mt-10">
+    <strong>Other videos showing how our simulation and real world setup evolved over time</strong>
   </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/Fwy_5q_lkCc?si=f2S7TXlu_h8KGwp8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-  <p class="mt-5">
-    MQP trailer!
-  </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/qo-FxxT3i9I?si=ZwVZQsm1Or8yNvoo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <div>
+    <p class="mt-5 text-center">
+      Heuristic 1 + ORCA + Stochastic Failure Demo!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/JnMw7biMyI8?si=eDI_XFZrclZnSJch" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
 
-  <p class="mt-5">
-    Heuristic1 + Recalc Full Demo!
-  </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/V9ycTuafOX4?si=MW9DSdsO1TOV5QEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <div>
+    <p class="mt-5 text-center">
+      Heuristic 1 + ORCA + Deadlock Resolution Demo!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/Fwy_5q_lkCc?si=f2S7TXlu_h8KGwp8" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
 
-  <p class="mt-5">
-    1st working demo (birds eye view)!
-  </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/qBHlO6XKXPU?si=Ub6JxwtnXRIW770F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <div>
+    <p class="mt-5 text-center">
+      Heuristic1 + Recalc(Heuristic2) Full Demo (w/o better depot relocation)!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/V9ycTuafOX4?si=MW9DSdsO1TOV5QEA" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
 
-  <p class="mt-5">
-    MQP Demo In Progress!
+  <p class="text-xl mt-10">
+    <strong>Some behind the scenes!</strong>
   </p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/WjcR9yUZi2g?si=e-G56j7RHUu-T4yI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+  <div>
+    <p class="mt-5 text-center">
+      MQP trailer!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/qo-FxxT3i9I?si=ZwVZQsm1Or8yNvoo" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
+
+  <div>
+    <p class="mt-5 text-center">
+      MQP Demo In Progress (Behind the scenes for one of the three runs)!
+    </p>
+    <div style="position:relative;padding-top:56.25%;">
+      <iframe src="https://www.youtube.com/embed/WjcR9yUZi2g?si=e-G56j7RHUu-T4yI" frameborder="0" allowfullscreen
+              style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
+  </div>
+
 
 </template>
 
