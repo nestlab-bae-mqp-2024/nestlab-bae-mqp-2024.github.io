de8ed8dd367b210816bcfc1221f251d074a7d60b
Yaşar İdikut <yasar8000@gmail.com>
2024-04-22 07:11:32 -0400

    add sponsors to the video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,7 @@
         Summary demo video
       </p>
       <div style="position:relative;padding-top:56.25%;">
-        <iframe src="https://www.youtube.com/embed/gj_nY3BjXEs?si=6ZmWuWsSXYtflNm2?rel=0&autoplay=1" frameborder="0" allowfullscreen
+        <iframe src="https://www.youtube.com/embed/gshXCPnQC0k?si=77HQc3nzG3UILYXB?rel=0&autoplay=1" frameborder="0" allowfullscreen
                 style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
       </div>
     </div>
