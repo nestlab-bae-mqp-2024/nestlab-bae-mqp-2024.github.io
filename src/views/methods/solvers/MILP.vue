01b0285133be59b1c0dd87b6dd6cdbc9ace2b109
Yaşar İdikut <yasar8000@gmail.com>
2024-04-25 06:06:29 -0400

    update github links

--- src/views/methods/solvers/MILP.vue
+++ src/views/methods/solvers/MILP.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-xl m-10">
     <p class="mb-5">
-      We have a Jupyter Notebook for this! Click <a class="underline hover:no-underline" target="_blank" href="https://github.com/wpi-nest-lab-bae-mqp-2023/milp-intro/blob/main/src/mrpcp_2015/attempts-and-improvements/2015_mrpcp_yasars_attempt_w_redundancy_param.ipynb">here</a> to see on GitHub, or see below.
+      We have a Jupyter Notebook for this! Click <a class="underline hover:no-underline" target="_blank" href="https://github.com/nestlab-bae-mqp-2024/mrpcp-solvers/blob/main/src/mrpcp_2015/attempts-and-improvements/2015_mrpcp_yasars_attempt_w_redundancy_param.ipynb">here</a> to see on GitHub, or see below.
     </p>
 
     <div id="milp_ipynb" ref="milp_ipynb" class="text-xs"></div>
@@ -17,7 +17,7 @@ export default {
   },
   async created () {
     nb.executeJavaScript = true;
-    const response = await fetch('https://raw.githubusercontent.com/wpi-nest-lab-bae-mqp-2023/milp-intro/main/src/mrpcp_2015/attempts-and-improvements/2015_mrpcp_yasars_attempt_w_redundancy_param.ipynb');
+    const response = await fetch('https://raw.githubusercontent.com/nestlab-bae-mqp-2024/mrpcp-solvers/main/src/mrpcp_2015/attempts-and-improvements/2015_mrpcp_yasars_attempt_w_redundancy_param.ipynb');
     while (this.$refs.milp_ipynb.hasChildNodes()) {
       this.$refs.milp_ipynb.removeChild(this.$refs.milp_ipynb.lastChild);
     }
