c176fd05d64d13bde86efc2fface61d477463960
Yaşar İdikut <yasar8000@gmail.com>
2024-04-25 06:06:40 -0400

    update home video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,7 @@
         Summary demo video
       </p>
       <div style="position:relative;padding-top:56.25%;">
-        <iframe src="https://www.youtube.com/embed/gshXCPnQC0k?si=77HQc3nzG3UILYXB?rel=0&autoplay=1" frameborder="0" allowfullscreen
+        <iframe src="https://www.youtube.com/embed/P3pQXTt6Ijk?si=i5IrsHSUcpOyboAQ?rel=0&autoplay=1" frameborder="0" allowfullscreen
                 style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
       </div>
     </div>
