6bd2d2013fcdccd4f48d9e9d510e600f7395f722
Yaşar İdikut <yasar8000@gmail.com>
2024-05-07 11:43:48 -0400

    rerun h1 titles

--- src/views/methods/solvers/Heuristic1.vue
+++ src/views/methods/solvers/Heuristic1.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="text-xl m-10">
-    <p class="mb-5">
+  <div class="text-xl m-10 grid grid-cols-1 divide-y">
+    <div class="mb-5">
       We have a Jupyter Notebook for this! Click <a class="underline hover:no-underline" target="_blank" href="https://github.com/nestlab-bae-mqp-2024/mrpcp-solvers/blob/main/src/heuristic_attempts/yasars_heuristic_attempts/yasars_heuristic_attempt.ipynb">here</a> to see on GitHub, or see below.
-    </p>
+    </div>
 
-    <div id="h1_ipynb" ref="h1_ipynb" class="text-xs"></div>
+    <div id="h1_ipynb" ref="h1_ipynb" class="text-xs pt-5"></div>
 
   </div>
 </template>
@@ -17,10 +17,8 @@ export default {
   },
   async created () {
     nb.executeJavaScript = true;
+    // const response = await fetch('http://localhost:5173/public/Users/yasaridikut/WebstormProjects/mqp-website/public/py/src/heuristic_attempts/yasars_heuristic_attempts/yasars_heuristic_attempt.ipynb');
     const response = await fetch('https://raw.githubusercontent.com/nestlab-bae-mqp-2024/mrpcp-solvers/main/src/heuristic_attempts/yasars_heuristic_attempts/yasars_heuristic_attempt.ipynb');
-    while (this.$refs.h1_ipynb.hasChildNodes()) {
-      this.$refs.h1_ipynb.removeChild(this.$refs.h1_ipynb.lastChild);
-    }
     const data = await response.json()
     this.$refs.h1_ipynb.appendChild(nb.parse(data).render());
     Prism.highlightAll()
@@ -30,7 +28,3 @@ export default {
 }
 
 </script>
-
-<style scoped>
-
-</style>
